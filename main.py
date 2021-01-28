81db66c1b22bb4f542e40d804ae54ad3d1044859
Matteo Milani <m.milani@aep-italia.it>
2021-01-28 23:37:21 +0100

    Rimosso filtro di ricerca per task con assegnazione temporale. Non tutti i task hanno tempo scaricato sora.

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ from content_provider.jira_content_provider import JIRAContentProvider, JiraPage
 searches = [
     {"product": "PROM", "account": "Progetto Prometheus"},
     {"product": "PROM", "account": "Addendum - I"},
-    {"product": "I285", "account": "2019-11-01 - Innocenti 285"},
-    {"product": "I285", "account": "I285 - Adendum Mailing "},
-    {"product": "I285", "account": "I285 - Addendum Rendicontazione"},
 
-    #{"product": "AWS", "account": "082018021AIP"},
-    #{"product": "AWS", "account": "082018021AIPCON"},
-    #{"product": "AWS", "account": "09-2020040"},
-    #{"product": "AWS", "account": "AIPCARNET01"},
-    #{"product": "AWS", "account": "AIP-NOASS"},
+    #{"product": "I285", "account": "2019-11-01 - Innocenti 285"},
+    #{"product": "I285", "account": "I285 - Adendum Mailing "},
+    #{"product": "I285", "account": "I285 - Addendum Rendicontazione"},
+#
+    #{"product": "AWS", "account": "08_20180219_FB_InformaticaComTofani - Progetto Turni Lavoro"},
+    #{"product": "AWS", "account": "09-20200406-CS-AIP-nuove sviluppi e canone di manutenzione v2"},
 ]
 
 jira: JIRAContentProvider = JIRAContentProvider("https://giuneco.atlassian.net", "matteo.milani@giuneco.it", "zDm2VCHLFWLJcESwXdea3AF3")
 
 for s in searches:
-    result: JiraPage = jira.search(f'project = "{s["product"]}" AND Account = "{s["account"]}" AND timespent > 0 ORDER BY created DESC')
+    result: JiraPage = jira.search(f'project = "{s["product"]}" AND Account = "{s["account"]}" ORDER BY created DESC')
+    #result: JiraPage = jira.search(f'project = "{s["product"]}" AND timespent > 0 ORDER BY created DESC')
 
     totalSeconds: int = 0
     for itm in result.items:
-        totalSeconds += itm.timeSpent
+        if itm.timeSpent is not None:
+            totalSeconds += itm.timeSpent
 
-    print(f'[{s["product"]}/{s["account"]}] Worked {result.total} tasks: {totalSeconds/60/60}h spent')
+    print(f'[{s["product"]}/{s["account"]}] Worked {result.total} tasks: {totalSeconds/60/60}h spent, TotH/Tasks: {totalSeconds/result.total/60/60}')
