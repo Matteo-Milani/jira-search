466b9aaed7f85d80df4c6e0a88a990b48ab6f702
Matteo Milani <m.milani@aep-italia.it>
2021-01-28 15:01:00 +0100

    Allineamento da portatile

--- content_provider/jira_content_provider.py
+++ content_provider/jira_content_provider.py
@@ -1,31 +1,41 @@
 import json
-from typing import List
 
 import requests
 from requests.auth import HTTPBasicAuth
+
+from typing import List
 
 
 class UnauthorizedError(Exception):
     def __init__(self, msg: str) -> None:
         self.message: str = msg
 
+
+class MergeError(Exception):
+    def __init__(self, msg: str) -> None:
+        self.message: str = msg
+
+
 class JiraItem:
     def __init__(self) -> None:
-        self.key: str = ""
-        self.timeSpent: int = 0
-        self.summary: str = ""
-        self.resolutionDate: str = ""
-        self.created: str = ""
-        self.status: str = ""
+        self.id: str
+        self.key: str
+        self.timeSpent: int
+        self.status: str
+        self.summary: str
+
 
 class JiraPage:
     def __init__(self) -> None:
-        self.total: int = 0
         self.items: List[JiraItem] = []
+        self.total: int = 0
 
-    def append(self, otherPage: 'JiraPage') -> None:
-        for itm in otherPage.items:
-            self.items.append(itm)
+    def merge(self, other: 'JiraPage') -> None:
+        if self.total != other.total:
+            raise MergeError("Can't merge two result sets from different queries")
+
+        for item in other.items:
+            self.items.append(item)
 
 
 class JIRAContentProvider:
@@ -34,9 +44,9 @@ class JIRAContentProvider:
         self._auth = HTTPBasicAuth(userName, password)
         self._pageSize = 50
 
-    def search(self, jql: str, start: int = 0) -> JiraPage:
+    def search(self, jql: str, start: int = 0) -> List[JiraPage]:
         url = f"{self._baseUrl}/rest/api/latest/search?maxResults={self._pageSize}&startAt={start}&jql={jql}"
-        print(f"[JIRA] Searching tasks: {url}")
+        #print(f"  [JIRA] Searching tasks: {url}")
 
         myResponse = requests.get(url, auth=self._auth)
         if myResponse.status_code == 401:
@@ -45,22 +55,21 @@ class JIRAContentProvider:
         myResponse.raise_for_status()  # Se call KO: raise exception
 
         jDoc = json.loads(myResponse.content)
-        # print(json.dumps(jDoc, indent=2))
+        #print(json.dumps(jDoc, indent=2))
 
-        currentPage: JiraPage = JiraPage()
-        currentPage.total = jDoc["total"]
+        jiraPage: JiraPage = JiraPage()
+        jiraPage.total = jDoc["total"]
 
         for issue in jDoc["issues"]:
-            itm: JiraItem = JiraItem()
-            itm.key = issue["key"]
-            itm.timeSpent = issue["fields"]["timespent"]
-            itm.summary = issue["fields"]["summary"]
-            itm.resolutionDate = issue["fields"]["resolutiondate"]
-            itm.created = issue["fields"]["created"]
-            itm.status = issue["fields"]["status"]["name"]
-            currentPage.items.append(itm)
+            item: JiraItem = JiraItem()
+            item.id = issue["id"]
+            item.key = issue["key"]
+            item.timeSpent = issue["fields"]["timespent"]
+            item.summary = issue["fields"]["summary"]
+            item.status = issue["fields"]["status"]["name"]
+            jiraPage.items.append(item)
 
-        if start + self._pageSize < currentPage.total:
-            currentPage.append(self.search(jql, start + self._pageSize))
+        if start + self._pageSize < jiraPage.total:
+            jiraPage.merge(self.search(jql, start + self._pageSize))
 
-        return currentPage
+        return jiraPage
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from typing import List
-
 from content_provider.jira_content_provider import JIRAContentProvider, JiraPage
 
-jira: JIRAContentProvider = JIRAContentProvider("https://giuneco.atlassian.net", "matteo.milani@giuneco.it",
-                                                "ZTiCDmrBtmPLokjPt8WR6FE9")
-# page: JiraPage = jira.search("project=PROM AND timespent > 0 AND Account='Progetto Prometheus' ORDER BY created DESC")
-# page: JiraPage = jira.search("project=PROM AND timespent > 0 ORDER BY created DESC")
-page: JiraPage = jira.search("project=PROM ORDER BY created DESC")
+searches = [
+    {"product": "PROM", "account": "Progetto Prometheus"},
+    {"product": "PROM", "account": "Addendum - I"},
+    {"product": "I285", "account": "2019-11-01 - Innocenti 285"},
+    {"product": "I285", "account": "I285 - Adendum Mailing "},
+    {"product": "I285", "account": "I285 - Addendum Rendicontazione"},
 
-workedSecs: int = 0
-for itm in page.items:
-    workedSecs += itm.timeSpent
+    #{"product": "AWS", "account": "082018021AIP"},
+    #{"product": "AWS", "account": "082018021AIPCON"},
+    #{"product": "AWS", "account": "09-2020040"},
+    #{"product": "AWS", "account": "AIPCARNET01"},
+    #{"product": "AWS", "account": "AIP-NOASS"},
+]
 
-workedHours: float = workedSecs / 3600
-workedDays: float = workedHours / 8
-print(f"For {page.total} logs we worked: {workedSecs}s -> {workedHours}h -> {workedDays}d")
+jira: JIRAContentProvider = JIRAContentProvider("https://giuneco.atlassian.net", "matteo.milani@giuneco.it", "zDm2VCHLFWLJcESwXdea3AF3")
+
+for s in searches:
+    result: JiraPage = jira.search(f'project = "{s["product"]}" AND Account = "{s["account"]}" AND timespent > 0 ORDER BY created DESC')
+
+    totalSeconds: int = 0
+    for itm in result.items:
+        totalSeconds += itm.timeSpent
+
+    print(f'[{s["product"]}/{s["account"]}] Worked {result.total} tasks: {totalSeconds/60/60}h spent')
